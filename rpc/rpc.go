d9b28712929861bef75c65cd0eebe83cccfc934e
philip glazman <8378656+philipglazman@users.noreply.github.com>
2021-08-30 21:01:00 -0700

    Init rpc

---
+++ rpc/rpc.go
@@ -0,0 +1,144 @@
+// Package rpc provides an RPC client to the Aleo Daemon.
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Config struct {
+	User string
+	Password string
+	Host string
+	Port string
+}
+
+type Client struct {
+	cfg *Config
+	httpClient *http.Client
+}
+
+// NewClient returns a new RPC client.
+func NewClient(cfg *Config) (*Client, error) {
+	// TODO Create an http.Client to speak.
+	httpClient := &http.Client{}
+
+	return &Client{
+		httpClient: httpClient,
+		cfg: cfg,
+	}, nil
+}
+
+func newRequest(client *Client, body []byte) ([]byte, error) {
+
+	url := fmt.Sprintf("http://%s:%s",client.cfg.Host, client.cfg.Port)
+
+	fmt.Printf("%v\n",body)
+	req, err := http.NewRequest("POST", url,bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("content-type", "application/json")
+
+	req.SetBasicAuth(client.cfg.User, client.cfg.Password)
+
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Handle response!
+	return ioutil.ReadAll(resp.Body)
+}
+
+type Request struct {
+	Method string `json:"method"`
+	Params []json.RawMessage `json:"params"`
+	Id string `json:"id"`
+	RpcVersion string `json:"jsonrpc"`
+}
+
+func newRequestBody(rpcVersion int, id string, method string, params []interface{}) ([]byte, error) {
+	req := &Request{
+		Method:     method,
+		Params:     []json.RawMessage{},
+		Id:         id,
+		RpcVersion: "2.0",
+	}
+	fmt.Printf("%v\n",req)
+	return json.Marshal(req)
+}
+
+
+type GetBlockCountResponse struct {
+	Result int64 `json:"result"`
+}
+
+// TODO decoderawtransaction
+
+// GetBlockCount returns the number of blocks in the best valid chain.
+// https://github.com/AleoHQ/snarkOS/blob/master/rpc/README.md#getblockcount
+func (c *Client) GetBlockCount() (int64, error) {
+	req, err := newRequestBody(2, "documentation","getblockcount" ,nil)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := newRequest(c, req)
+	if err != nil {
+		return 0, err
+	}
+
+	var count GetBlockCountResponse
+	if err := json.Unmarshal(resp, &count); err != nil {
+		return 0, err
+	}
+
+	return count.Result, nil
+}
+
+// GetBestBlockhash returns the block hash of the head of the best valid chain.
+func (c *Client) GetBestBlockhash() (string, error) {
+	req, err := newRequestBody(2, "documentation","getbestblockhash" ,nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := newRequest(c, req)
+	if err != nil {
+		return "", err
+	}
+
+	var res GetBestBlockhashResponse
+	if err := json.Unmarshal(resp, &res); err != nil {
+		return "", err
+	}
+
+	return res.Result, nil
+}
+
+func (c *Client) GetBlock() (string, error) {
+	req, err := newRequestBody(2, "documentation","getblock" ,nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := newRequest(c, req)
+	if err != nil {
+		return "", err
+	}
+
+	var res GetBestBlockhashResponse
+	if err := json.Unmarshal(resp, &res); err != nil {
+		return "", err
+	}
+
+	return res.Result, nil
+}
+
+// TODO getblock
+
